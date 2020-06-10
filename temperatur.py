d87191a822784d283c8e22db2881ff026959ff1a
phoiniks <phoiniks@grellopolis.de>
2020-06-10 12:20:27 +0200

    Laufzeit (Dauer in Minuten) wird nun als Parameter übergeben.

--- temperatur.py
+++ temperatur.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+from os import remove
+from os.path import exists
 from socket import gethostname
 from subprocess import check_output
+from sys import argv
 from sqlite3 import connect
 from time import sleep
+
+if exists("computer.db"):
+    remove("computer.db")
 
 con = connect("computer.db")
 cur = con.cursor()
@@ -15,9 +21,15 @@ cur.execute(create)
 
 insert = "INSERT INTO {} (temperatur) VALUES(?)".format(tabelle)
 
-while True:
+# Laufzeit in Minuten als Parameter, z.B.: ./temperatur.py 10 # Programm läuft 10 Minuten
+zeit = int(argv[1]) * 60
+
+print("Laufzeit von {:s}: {:d}\n".format(argv[0][2:], zeit))
+
+for s in range(zeit):
     temperatur = check_output(["cat", "/sys/class/thermal/thermal_zone0/temp"])
     temperatur = float("{:2.2f}".format(float(temperatur.decode("utf-8")) / 1000))
+    print("\r{}".format(temperatur), end='')
     cur.execute(insert, (temperatur,))
     sleep(1)
     con.commit()
